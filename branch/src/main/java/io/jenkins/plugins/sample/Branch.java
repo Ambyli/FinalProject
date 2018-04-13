7baf88f5ad29e1942e0ff9e1416f9d74591d0600
Blake Price <bllpr15@gmail.com>
2018-04-13 12:39:19 -0400

    updated from vm instead of local machine

---
+++ branch/src/main/java/io/jenkins/plugins/sample/Branch.java
@@ -0,0 +1,67 @@
+//This file will be used to locally change the git branch to be built
+package io.jenkins.plugins.sample;
+
+import java.lang.Runtime;
+import hudson.Launcher;
+//import hudson.Extension;
+import hudson.FilePath;
+//import hudson.util.FormValidation;
+//import hudson.model.AbstractProject;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import hudson.tasks.Builder;
+//import hudson.tasks.BuildStepDescriptor;
+import org.kohsuke.stapler.DataBoundConstructor;
+//import org.kohsuke.stapler.QueryParameter;
+
+//import javax.servlet.ServletException;
+import java.io.IOException;
+import jenkins.tasks.SimpleBuildStep;
+//import org.jenkinsci.Symbol;
+//import org.kohsuke.stapler.DataBoundSetter;
+import java.io.InputStream;
+
+public class Branch extends Builder implements SimpleBuildStep {
+
+	private String branch;
+    private String runCommand;
+    
+    public Branch() {
+    	this.branch = "master"; //default branch is usually master
+    	this.runCommand = "git checkout" + branch;
+    }
+
+    @DataBoundConstructor
+    public Branch(String branch) {
+        this.branch = branch;
+        this.runCommand = "git checkout" + branch;
+    }
+
+    public String getCommand() {
+        return runCommand;
+    }
+    
+    public String getBranch() {
+    	return branch;
+    }
+    
+    public String execBranchSearch() throws IOException {
+    	String command = "git branches -a";
+		Process child = Runtime.getRuntime().exec(command);
+    	InputStream in = child.getInputStream();
+    	/*int c;
+    	while((c = in.read()) != -1)
+    	{
+    		System.out.println((char) c);
+    	}*/
+    	in.close();
+    	return null;
+    }
+
+    @Override
+    public void perform(Run<?, ?> run, FilePath workspace, Launcher launcher, TaskListener listener) throws InterruptedException, IOException {
+    	run.addAction(new BranchAction("Current Branch: " + branch));
+    	Runtime rt = Runtime.getRuntime();
+       	Process pr = rt.exec(runCommand); //pr doesnt need to be used so ignore warning
+    }
+}
---
+++ branch/src/test/java/io/jenkins/plugins/sample/BranchTest.java
@@ -0,0 +1,5 @@
+package io.jenkins.plugins.sample;
+
+public class BranchTest {
+	//do nothing at the moment :D
+}
