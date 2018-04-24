5c959c5177569838371fddfb8f097ecb9c09a0a0
Blake <bllpr15@gmail.com>
2018-04-24 16:38:29 -0400

    fixes

--- branch/src/main/java/io/jenkins/plugins/sample/BranchBuilder.java
+++ branch/src/main/java/io/jenkins/plugins/sample/BranchBuilder.java
@@ -19,18 +19,18 @@ import org.kohsuke.stapler.QueryParameter;
 
 import javax.servlet.ServletException;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
+//import java.io.BufferedWriter;
+//import java.io.File;
+//import java.io.FileNotFoundException;
+//import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.nio.file.Files;
+//import java.io.PrintWriter;
+//import java.io.UnsupportedEncodingException;
+//import java.nio.file.Files;
 
 import jenkins.tasks.SimpleBuildStep;
 import org.jenkinsci.Symbol;
-import org.kohsuke.stapler.DataBoundSetter;
+//import org.kohsuke.stapler.DataBoundSetter;
 
 public class BranchBuilder extends Builder implements SimpleBuildStep {
 
@@ -57,7 +57,6 @@ public class BranchBuilder extends Builder implements SimpleBuildStep {
     	ProcStarter ps = launcher.new ProcStarter();
     	ps = ps.cmds(command).stdout(listener);
     	ps = ps.pwd(build.getWorkspace()).envs(build.getEnvironment(listener));
-    	@SuppressWarnings("null")
 		Proc proc = launcher.launch(ps);
     	int retcode = proc.join();
     	
